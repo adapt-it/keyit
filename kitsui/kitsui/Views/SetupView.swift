8ce4a911e34661f03110619bd0d0bffed3b42811
Graeme Costin <91712092+GraemeAdaptit@users.noreply.github.com>
2024-01-04 22:24:41 +1100

    Version info added to SetupView

--- kitsui/kitsui/Views/SetupView.swift
+++ kitsui/kitsui/Views/SetupView.swift
@@ -11,6 +11,7 @@
 //	May you share freely, never taking more than you give.
 
 import SwiftUI
+import Foundation
 
 struct SetupView: View {
     @EnvironmentObject var bibMod: BibleModel
@@ -24,11 +25,33 @@ struct SetupView: View {
 		self._editedName = State(wrappedValue: bibleName)
 		self.needSetup = needSetup
 		self._goChooseBook = State(wrappedValue: !needSetup)
+		showAppVersion()
 	}
 
 	func getBibleName () -> String {
 		@EnvironmentObject var bibMod: BibleModel
 		return bibMod.getCurBibName()
+	}
+
+	func getAppVersion() -> String {
+		var appVerText: String
+		if let appVersion = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String {
+			appVerText = "App version: \(appVersion)"
+			if let buildNumber = Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as? String {
+				appVerText = appVerText + " (" + buildNumber + ")"
+			}
+		} else {
+			appVerText = "App version not available."
+		}
+		return appVerText
+	}
+	
+	func showAppVersion() {
+		if let appVersion = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String {
+			print("App version: \(appVersion)")
+		} else {
+			print("your platform does not support this feature.")
+		}
 	}
 
     var body: some View {
@@ -57,6 +80,8 @@ struct SetupView: View {
                     goChooseBook = true
                 }
                 Spacer()
+				Text("\(getAppVersion())")
+					.font(.system(size: 10.0))
             }
 			.navigationDestination(isPresented: $goChooseBook){
 				ChooseBookView(needChooseBook: (bibMod.getCurBibInst().currBk == 0)).environmentObject(bibMod)
